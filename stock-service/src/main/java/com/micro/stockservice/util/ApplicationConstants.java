b38abf405b3bb0fac33572048c15943007fd6124
Ayoub Khial <ayouub.khial@gmail.com>
2018-10-04 19:01:23 +0100

    Define application constants

---
+++ stock-service/src/main/java/com/micro/stockservice/util/ApplicationConstants.java
@@ -0,0 +1,11 @@
+package com.micro.stockservice.util;
+
+public final class ApplicationConstants {
+
+    public static final String DEFAULT_PAGE_NUMBER = "0";
+    public static final String DEFAULT_PAGE_SIZE = "20";
+    public static final int MAX_PAGE_SIZE = 50;
+
+    // restrict instantiation
+    private ApplicationConstants() {}
+}
---
+++ stock-service/src/main/java/com/micro/stockservice/util/HTTPCode.java
@@ -0,0 +1,15 @@
+package com.micro.stockservice.util;
+
+public final class HTTPCode {
+	public static final Pair<Integer, String> OK = new Pair<>(200, "OK");
+	public static final Pair<Integer, String> CREATED = new Pair<>(201, "Created");
+	public static final Pair<Integer, String> NOT_MODIFIED = new Pair<>(304, "Not modified");
+	public static final Pair<Integer, String> BAD_REQUEST = new Pair<>(400, "Bad request");
+	public static final Pair<Integer, String> UNAUTHORIZED = new Pair<>(401, "Unauthorized");
+	public static final Pair<Integer, String> FORBIDDEN = new Pair<>(403, "Forbidden");
+	public static final Pair<Integer, String> NOT_FOUND = new Pair<>(404, "Not found");
+	public static final Pair<Integer, String> INTERNAL_SERVER_ERROR = new Pair<>(500, "Internal Server Error");
+
+	// restrict instantiation
+	private HTTPCode() {}
+}
